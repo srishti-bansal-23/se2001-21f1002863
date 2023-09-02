34ed1ef7a5e0763cdde31deda9838aa890ae8988
21f1002863@se2001 <21f1002863@se2001.ds.study.iitm.ac.in>
2023-09-02 13:36:03 +0000

    added script.sh

--- script.sh
+++ script.sh
@@ -1,12 +1,7 @@
-<<<<<<< HEAD
 #!/bin/bash
 
-=======
->>>>>>> 82fa170e25174a47ca29e32046c16f98784ec77a
 # Export the GitHub username
 export GITHUB_USERNAME="srishti-bansal-iit"
 
 # Export the GitHub repository name
-export GITHUB_REPOSITORY="se2001-21f1002863"
-
-
+export GITHUB_REPOSITORY="se2001-se2001-21f1002863"
